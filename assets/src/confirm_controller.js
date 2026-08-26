fd12715aa9f7ffd4c268a2906d51eb6ddceb0704
Karel Scheers <karel@tbd.be>
2026-08-26 09:58:41 +0200

    Add stream value support to confirm controller for conditional Turbo Stream handling
    Add stream boolean value to controller, conditionally set or remove data-turbo-stream attribute on confirm link based on streamValue.

--- assets/src/confirm_controller.js
+++ assets/src/confirm_controller.js
@@ -5,6 +5,7 @@ export default class extends Controller {
     static values = {
         id: {type: String, default: 'modal-confirm'},
         path: String,
+        stream: Boolean,
     }
 
     modal() {
@@ -15,6 +16,12 @@ export default class extends Controller {
         if (modal) {
             let confirm = modal.querySelector('.confirm')
             confirm.setAttribute('href', path)
+
+            if (this.streamValue) {
+                confirm.setAttribute('data-turbo-stream', '')
+            } else {
+                confirm.removeAttribute('data-turbo-stream')
+            }
         }
     }
 }
